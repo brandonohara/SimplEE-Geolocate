e04022d13f55aa83c3a24e414805df34289c0a10
Brandon O'Hara <brandon@brandonohara.com>
2015-03-11 13:13:04 -0600

    Major updates, change geo lookup to serverside

--- ft.simplee_geolocate.php
+++ ft.simplee_geolocate.php
@@ -2,27 +2,38 @@
 
 	class Simplee_geolocate_ft extends EE_Fieldtype {
 	
+		var $has_array_data = TRUE;
 		var $info = array(
 			'name' => 'SimplEE Geolocate',
-			'version' => '1.0.0'
+			'version' => '1.0.1'
 		);
 		
 		var $geo_fields = array(
-			'address' => '',
-			'latitude' => 0.0, 
-			'longitude' => 0.0
+			"address"			=> "",
+			"number"			=> "",
+			"street_number"		=> "",
+			"street_name"		=> "",
+			"neighborhood"		=> "",
+			"city"				=> "",
+			"state"				=> "",
+			"county"			=> "",
+			"zip"				=> "",
+			"country"			=> "",
+			"latitude"			=> "",
+			"longitude"			=> ""
 		);
 		
-		var $has_array_data = TRUE;
-		
-		function _include_head(){
-			$themes = defined('URL_THIRD_THEMES') ? URL_THIRD_THEMES : get_instance()->config->slash_item('theme_folder_url').'third_party/';
-			$themes = $themes.'simplee_geolocate/';
-			
-			ee()->cp->add_to_head('<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>');
-			ee()->cp->add_to_head('<link type="text/css" rel="stylesheet" href="'.$themes.'css/cp.css" media="screen" />');
-			ee()->cp->add_to_head('<script type="text/javascript" src="'.$themes.'javascript/cp.js"></script>');
-		}
+		var $address_components = array(
+			"subpremise"					=> "number",
+			"street_number"					=> "street_number",
+			"route"							=> "street_name",
+			"neighborhood"					=> "neighborhood",
+			"locality"						=> "city",
+			"administrative_area_level_1"	=> "state",
+			"administrative_area_level_2"	=> "county",
+			"country"						=> "country",
+			"postal_code"					=> "zip"
+		);
 		
 		function install(){
 			return array(
@@ -41,99 +52,113 @@
 			return array_merge($this->settings, $_POST);
 		}
 		
-		function display_field($data){
-			$out = '';
-			$key = $this->settings['key'];
-			$data = ($data ? unserialize(base64_decode($data)) : $this->geo_fields);
-			$this->_include_head();
-			
-	
-			foreach($data as $key => $value){
-				$name = $key.'_field_id_'.$this->field_id;
-				
-				$out .= "<div class='simplee_geolocate_field' ".($key != "address" ? "style='display: none;'" : "").">";
-				//$out .= form_label(ucfirst($key), $name);
-				$out .= form_input(array(
-					'id' 	=>	$name,
-					'name' 	=> 	$name,
-					'value'	=>	$value
-				));
-				$out .= "</div>";
-			}
-			
-			$out .= "<div class='simplee_geolocate_field'>";
-			$out .= "<a onclick='SimpleeGeolocate(".$this->field_id.")' class='simplee_geolocate_button'>Geolocate</a>";
-			$out .= "</div>";
-			$out .= "<div style='clear: both;'></div>";
-			$out .= "<p class='simplee_geolocate_message' id='message_field_id_".$this->field_id."'></p>";
-			
-			return $out;
+		function display_settings(){
+			ee()->table->add_row(
+				"Update Past Entries?",
+				form_dropdown('update_entries', array('no' => "No", 'yes' => "Yes"))
+			);
 		}
 		
-		function save($data){
-			$data = array();
-			foreach($this->geo_fields as $key => $value){
-				$data[$key] = ee()->input->post($key."_field_id_".$this->field_id);
+		function save_settings($data){
+			$update = ee()->input->post('update_entries');
+			$field_id = "field_id_".$data['field_id'];
+			$field_id_copy = $field_id; //"field_id_2";
+			
+			if($update == "yes" && $field_id){
+				$query = ee()->db->where($field_id." IS NOT NULL", null, false)->from("channel_data")->get();
+				foreach($query->result_array() as $row){
+					$entry_id = $row['entry_id'];
+					$old = $row[$field_id];
+					
+					if(base64_encode(base64_decode($old, true)) !== $old){
+						//address is just saved as a string
+						$data = $this->_get_address_data($old);
+					} else {
+						//update from previous version
+						$data = unserialize(base64_decode($old));
+						$data = $this->_get_address_data($data['address']);
+					}
+					$data = base64_encode(serialize($data));
+					ee()->db->where("entry_id", $entry_id)->update("channel_data", array($field_id => $data));
+				}
 			}
+			
+		    return array();
+		}
+		
+		function display_field($data){
+			$data = ($data ? unserialize(base64_decode($data)) : $this->geo_fields);
+			
+			$name = 'field_id_'.$this->field_id;
+			$value = $data['address'];
+			
+			return form_input(array(
+				'id' 	=>	$name,
+				'name' 	=> 	$name,
+				'value'	=>	$value
+			));
+		}
+		
+		
+		function save($address){
+			//$data = ($data != '' ? unserialize(base64_decode($data)) : $this->geo_fields);
+			$data = $this->_get_address_data($address);
 			return base64_encode(serialize($data));
+		}
+		
+		function _get_address_data($address){
+			$data = $this->geo_fields;
+			$data['address'] = $address;
+			
+			
+			$gm_address = str_replace(' ','+',$address);
+		    $geocode = file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$gm_address.'&sensor=false');
+		    $output = json_decode($geocode);
+		    
+		    if($output->status != "OK"){
+			    return $data;
+		    }
+		    
+		    $result = $output->results[0];
+		    foreach($result->address_components as $component){
+			    $value = $component->long_name;
+			    foreach($component->types as $type){
+				    if( isset($this->address_components[$type]) ){
+					    $data[$this->address_components[$type]] = $value;
+				    }	
+			    }
+		    }
+		     
+		    $data['formatted'] = $result->formatted_address;  
+		    $data['latitude'] = $result->geometry->location->lat;
+		    $data['longitude'] = $result->geometry->location->lng;
+		    
+		    return $data;
 		}
 		
 		
 		function replace_tag($data, $params = array(), $tagdata = FALSE){
 			$data = unserialize(base64_decode($data));
-			
-			if($tagdata){
-				return ee()->TMPL->parse_variables_row($tagdata, $data);
-			} else
-				return "Address: ".$data['address']." (".$data['longitude'].", ".$data['longitude'].")";
+			return ee()->TMPL->parse_variables_row($tagdata, $data);
 		}
 		
-		
-		
-		/* ========================================================
-		=========================   MATRIX   ======================
-		
-		function randomString($length = 10) {
-		    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		    $randomString = '';
-		    for ($i = 0; $i < $length; $i++) {
-		        $randomString .= $characters[rand(0, strlen($characters) - 1)];
-		    }
-		    return $randomString;
-		}
-		
+		/*
 		function display_cell( $data ){
-	    	$out = '';
-			$key = $this->settings['key'];
-			$data = ($data ? unserialize(base64_decode($data)) : $this->geo_fields);
-			$this->_include_head();
+	    	$data = ($data ? unserialize(base64_decode($data)) : $this->geo_fields);
 			
-			foreach($data as $key => $value){
-				$name = $key.'_field_id_'.$this->field_id;
-				
-				$out .= "<div class='geolocatee_field' ".($key != "address" ? "style='display: none;'" : "").">";
-				//$out .= form_label(ucfirst($key), $name);
-				$out .= form_input(array(
-					'id' 	=>	$name,
-					'name' 	=> 	$name,
-					'value'	=>	$value
-				));
-				$out .= "</div>";
-			}
+			$name = 'field_id_'.$this->field_id;
+			$value = $data['address'];
 			
-			$out .= "<div class='geolocatee_field'>";
-			$out .= "<a onclick='GeolocatEEMatrix($(this).parent())' class='geolocatee_button'>Geolocate</a>";
-			$out .= "</div>";
-			$out .= "<div style='clear: both;'></div>";
-			$out .= form_hidden($this->field_name, implode("|", $data))
-			$out .= "<p class='geolocatee_message' id='message_field_id_".$unique."'>".implode("|", $data)."</p>";
-			
-			return $out;
+			return form_input(array(
+				'id' 	=>	$name,
+				'name' 	=> 	$name,
+				'value'	=>	$value
+			));
 	    }
 	    
-	    function save_cell($data){
-		    $data = explode("|", $data);
+	    function save_cell($address){
+		    $data = $this->_get_address_data($address);
 			return base64_encode(serialize($data));
 	    }
-		=========================================================== */
+	    */
 	}
